e0d1361a2cd795a8b764a18aaaeca2ec394f9587
Eryk Szyfner <eryk.szyfner@gmail.com>
2024-02-05 19:33:51 +0100

    Simplify lualine setup options

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,6 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        -- TODO: this can be simplified with "opts"
-        require("lualine").setup({
-            options = {
-                theme = "dracula"
-            }
-        })
-    end
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- requires nerd fonts in the terminal to work correctly
+    opts = { options = { theme = "dracula" } }
 }
 
