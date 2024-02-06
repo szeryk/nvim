119b6d36f99134fe1de74f1e654699ab11080a16
Eryk Szyfner <eryk.szyfner@gmail.com>
2024-02-06 15:26:50 +0100

    Add support for formatters and linters
    - none-ls is used for universal and easy formatters/linters config
    - formatters and linters need to be downloaded manually with Mason

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,21 @@
+return {
+	-- none-ls is fork of null-ls - a tool that wraps formatters and linters
+	-- into a lsp-like server, which makes managing them much easier
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+                -- Warning: those are not installed automatically by Mason
+                -- need to do it maually with :Mason
+                -- Mason relies on different host apps like: pip, venv, npm etc.
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+                -- for linters it goes like this:
+                -- null_ls.builtins.diagnostics.eslint_d,
+			},
+		})
+	end,
+}
--- lua/vim-keymap.lua
+++ lua/vim-keymap.lua
@@ -2,50 +2,49 @@
 local K = {}
 
 function K.setGeneralKeymap()
-    -- my leader is SPACE
-    vim.g.mapleader = " "
+	-- my leader is SPACE
+	vim.g.mapleader = " "
 end
 
 function K.setNeotreeKeymap()
-    vim.keymap.set("n", "<leader>tt", ":Neotree filesystem reveal left<CR>")
+	vim.keymap.set("n", "<leader>tt", ":Neotree filesystem reveal left<CR>")
 end
 
 function K.setTelescopeKeymap()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+	local builtin = require("telescope.builtin")
+	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+	vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 end
 
 function K.setLspKeymap()
-    vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+		callback = function(ev)
+			-- Enable completion triggered by <c-x><c-o>
+			vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-            -- Buffer local mappings.
-            -- See `:help vim.lsp.*` for documentation on any of the below functions
-            local opts = { buffer = ev.buf }
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-            vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set('n', '<leader>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts)
-            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', '<leader>=', function()
-                vim.lsp.buf.format { async = true }
-            end, opts)
-        end,
-    })
-
+			-- Buffer local mappings.
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			local opts = { buffer = ev.buf }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, opts)
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<leader>=", function()
+				vim.lsp.buf.format({ async = true })
+			end, opts)
+		end,
+	})
 end
 
 return K
