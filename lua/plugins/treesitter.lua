66027555b484b72e3382c358bd5f24b1a34a7977
Eryk Szyfner <eryk.szyfner@gmail.com>
2025-01-15 22:25:52 +0100

    Add markdown support to TreeSitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,14 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = {"lua", "python", "c", "cpp", "xml"},
-            highlight = { enable = true },
+            ensure_installed = {"lua", "python", "c", "cpp", "xml", "markdown"},
+            highlight = {
+                enable = true,
+                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+                --  If you are experiencing weird indenting issues, add the language to
+                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+                additional_vim_regex_highlighting = { 'ruby', 'markdown' },
+            },
             indent = { enable = true },
         })
     end
