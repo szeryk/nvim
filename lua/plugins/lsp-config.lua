d84bc74fcd213e4d13ecffb3c2509412b019d59b
Eryk Szyfner <eryk.szyfner@gmail.com>
2025-10-04 09:56:55 +0200

    Change LSP config to use nvim 0.11 features
    vim.lsp.config() and vim.lsp.enable() is the new API to configure
    and start LSPs. No need for the old nvim-lspconfig framework with
    its "setup" calls. The plugin is still useful tho as it keeps the
    default configs for many different LSPs and those are used by
    default if it's installed.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,51 +22,18 @@ return {
         config = function()
             -- capabilities are part of completions configuration - 
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            vim.lsp.config(
+                "*",
+                {
+                    capabilities = capabilities,
+                }
+            )
 
-            local lspconfig = require("lspconfig")
-            -- here setup all of the lsp you want to attach to
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using
-                            -- (most likely LuaJIT in the case of Neovim)
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` and Snacks global
-                            globals = {
-                                'vim',
-                                'require',
-                                'Snacks',
-                            },
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        -- Do not send telemetry data containing a randomized but unique identifier
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-
-            })
-
-            -- if pyright is not attached - check your npm and node version and:
-            -- :checkhealth mason
-            lspconfig.pyright.setup({
-                capabilities = capabilities
-            })
-
-            lspconfig.clangd.setup({
-                capabilities = capabilities
-            })
-
-            lspconfig.marksman.setup({
-                capabilities = capabilities
+            vim.lsp.enable({
+                "lua_ls",
+                "pyright",
+                "clangd",
+                "marksman",
             })
 
             local vimKeymap = require("..vim-keymap")
