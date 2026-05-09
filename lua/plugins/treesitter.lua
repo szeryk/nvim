7ea14ff9d3763394ff4816e1c080441ccbad4f7d
Eryk Szyfner <eryk@haleytek.com>
2026-05-09 16:30:00 +0200

    Update to use the new treesitter API
    Most notable changes:
    - nvim-treesitter.configs no longer exists
    - highlight is not ON by default, reguire setup for each buffer

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,18 +2,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.config")
-        configs.setup({
-            auto_install = true,
-            ensure_installed = {"lua", "python", "c", "cpp", "xml", "markdown"},
-            highlight = {
-                enable = true,
-                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                --  If you are experiencing weird indenting issues, add the language to
-                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-                additional_vim_regex_highlighting = { 'ruby', 'markdown' },
-            },
-            indent = { enable = true },
+        -- Install parsers
+        require("nvim-treesitter").install({"lua", "python", "c", "cpp", "xml", "markdown"})
+
+        -- Enable treesitter highlighting for filetypes with parsers
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
         })
     end
 }
